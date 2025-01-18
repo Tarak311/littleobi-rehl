bf7adceccfa6987b7a12722193a821f4f09d1efe
Tarak311 <tarak_1@live.com>
2025-01-18 17:07:10 -0500

    ddd

--- init.sh
+++ init.sh
@@ -59,3 +59,6 @@ net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 net.ipv4.ip_forward = 1
 EOF 
+
+kubectl get pvc -n vault | grep vault  | awk  '{print $2}' | xargs kubectl delete pvc  -n vault
+kubectl get pvc  |  awk  '{print $2}' | xargs kubectl delete pvc  
